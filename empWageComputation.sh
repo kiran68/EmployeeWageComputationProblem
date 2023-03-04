6f67de5e51d983e0fefa6e2832ef8f0736a87abe
unknown <kiranurshil88@gmail.com>
2023-03-04 14:31:19 +0530

    Add: Calculating Employee Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,13 +1,23 @@
 echo "Welcome Employee Wage Computation Problem"
 
+
 isPresent=1;
 randomCheck=$((RANDOM%2));
 
-if [ $isPresent -eq $randomCheck ];
-then 
-     echo "Employee is Present";
-else 
-     echo "Employee is Abscent";
+if [ $isPresent -eq $randomCheck ]
+
+then
+echo "Employee is present"
+empRatePerHr=20
+empHrs=8;
+salary=$(($empHrs*$empRatePerHr))
+echo "salary="$salary
+else
+echo "Employee is absent"
+echo "salary="0
+
 fi
+
+
 
 
