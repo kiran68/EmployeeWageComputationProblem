478f12f8baa517489bfcc6db2be74ded98a1f269
unknown <kiranurshil88@gmail.com>
2023-03-04 16:45:16 +0530

    Refector: Code to write a function to get work hours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,7 +1,5 @@
 echo "Welcome Employee Wage Computation Problem"
 
-
-
 isPartTime=1;
 isFullTime=2;
 empHrs=0;
@@ -11,28 +9,33 @@ empRatePerHr=20;
 numWorkingDays=20;
 day=1;
 
-while [[ $day -le 20 && $totalWorkingHour -lt 100 ]]
-do
-
-randomCheck=$((RANDOM%3));
-
-case $randomCheck in $isFullTime)
+function calculateWorkingHour() {
+	case $randomCheck in 
+                     $isFullTime)
                       empHrs=8;;
                      $isPartTime)
                       empHrs=4;;
                      *)
                       empHrs=0;;
 esac
-totalWorkingHour=$(($totalWorkingHour+$empHrs))
+	echo $empHrs;
+}
+while [[ $day -le 20 && $totalWorkingHour -lt 100 ]]
+do
+randomCheck=$((RANDOM%3));
 
-if [ $totalWorkingHour -gt 100 ]
-then
-totalWorkingHour=$(($totalWorkingHour-$empHrs))
-break;
-fi
-salary=$(($empRatePerHr*$empHrs))
-totalSalary=$(($totalSalary+$salary))
-((day++))
+	wHour=$(calculateWorkingHour $randomCheck);
+	totalWorkingHour=$(($totalWorkingHour + $wHour));
+	if [ $totalWorkingHour -gt 100 ]
+	then
+		totalWorkingHour=$(($totalWorkingHour - $wHour));
+		break;
+	fi
+	salary=$(($empRatePerHr*$wHour));
+	totalSalary=$(($totalSalary+$salary));
+	((day++));
 done
-
 echo "Employee has earned $totalSalary $ in a month (Total working Hour : $totalWorkingHour)";
+
+
+
